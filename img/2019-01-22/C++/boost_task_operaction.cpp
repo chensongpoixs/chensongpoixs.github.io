d8b57ed23614603ab62de0cb363099a9e37e4838
chensong <chensongbj25@gmail.com>
2019-01-23 07:57:43 +0800

    C++ call function

--- img/2019-01-22/C++/boost_task_operaction.cpp
+++ img/2019-01-22/C++/boost_task_operaction.cpp
@@ -19,13 +19,9 @@ public:
 	void complete(const int& ec, std::size_t bytes)
 	{
 		printf("[%s][%d]\n", __FUNCTION__, __LINE__);
-		func_(this, ec, bytes);
+		func_(this, ec, bytes);// call_back --> do_complete
 	}
-	void destroy()
-	{
-		printf("[%s][%d]\n", __FUNCTION__, __LINE__);
-		func_(this, int(), 0);
-	}
+	
 protected:
 	typedef void (*func_type)(operation *, const int&, std::size_t);
 	operation(func_type func) :func_(func)
@@ -36,42 +32,20 @@ private:
 	func_type	func_;
 };
 
-class reactor_op :public operation
-{
-public:
-	bool perform()
-	{
-		printf("[%s][%d]\n", __FUNCTION__, __LINE__);
-		return perform_func_(this);
-	}
-protected:
-	typedef bool(*perform_func_type)(reactor_op*);
-	reactor_op(perform_func_type perform_func, func_type complete_func)
-		:operation(complete_func),
-		perform_func_(perform_func) 
-	{
-		printf("[%s][%d]\n", __FUNCTION__, __LINE__);
-	}
-private:
-	perform_func_type	perform_func_;
-};
-
-
-class reactive_op_base : public reactor_op
+class reactive_op_base : public operation
 {
 public:
 	reactive_op_base(func_type complete_func)
-		: reactor_op(&reactive_op_base::do_perform, complete_func)
+		: operation(complete_func)
 	{
 		printf("[%s][%d]\n", __FUNCTION__, __LINE__);
 	}
 	//这里一直处理任务 直到任务处理完成结束提出
-	static bool do_perform(reactor_op * base)
+	static bool do_perform(operation * base)
 	{
 		reactive_op_base * r(static_cast<reactive_op_base*>(base));
 		// r->perform(); // cycle
 		printf("[%s][%d]function = %p\n", __FUNCTION__, __LINE__, r);
-		printf("[%s][%d]\n", __FUNCTION__, __LINE__);
 		static int i = 0;
 		if (i > 1000)
 		{
@@ -82,7 +56,7 @@ public:
 			printf("[%s][%d] i = %d\n", __FUNCTION__, __LINE__, i);
 			
 #ifdef _MSC_VER
-				Sleep(1000);
+			Sleep(1000);
 #elif defined(__GNUC__) 
 			sleep(1);
 #else
